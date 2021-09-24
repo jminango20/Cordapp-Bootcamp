39058e1c11a23be3c9d55070d9510241d2be9372
unknown <juan.negrete@bbchain.com.br>
2021-09-24 19:42:54 -0300

    webServer

--- clients/src/main/kotlin/com/template/webserver/services/ReinsuranceApiServiceImpl.kt
+++ clients/src/main/kotlin/com/template/webserver/services/ReinsuranceApiServiceImpl.kt
@@ -1,13 +1,24 @@
 package com.template.webserver.services
 
+import com.template.flows.CreateFlow
+import com.template.states.ReinsuranceState
 import com.template.webserver.NodeRPCConnection
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.identity.Party
+import net.corda.core.utilities.getOrThrow
 import org.openapitools.api.ReinsuranceApiService
 import org.openapitools.model.*
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class ReinsuranceApiServiceImpl @Autowired constructor(rpc:NodeRPCConnection) : ReinsuranceApiService {
+
+    companion object{
+        private val logger = LoggerFactory.getLogger(ReinsuranceApiServiceImpl::class.java)
+    }
+
     private val proxy = rpc.proxy
     override fun reinsuranceGet(status: String): ListDetailResponse {
      var list = ListDetailResponse()
@@ -41,10 +52,29 @@ class ReinsuranceApiServiceImpl @Autowired constructor(rpc:NodeRPCConnection) : 
         )
     }
 
+    private fun getParty(partyName: String): Party {
+        return proxy.wellKnownPartyFromX500Name(CordaX500Name.parse(partyName))
+            ?: throw IllegalArgumentException("Unkown party name $partyName")
+    }
     override fun reinsurancePost(body: ReinsuranceRequest): ReinsuranceResponse {
-        return ReinsuranceResponse(
-            contractId = "12345",
-            ledgerHashKey = "23455566"
+        logger.info("Running reinsurancePost")
+
+        val reinsurer = getParty(body.reinsurer)
+        val register = getParty(body.register)
+
+        val flowHandle = proxy.startTrackedFlowDynamic(CreateFlow::class.java, reinsurer, register, body.contractId)
+
+        flowHandle.progress.subscribe {
+            logger.info("CreatedFlow: $it")
+        }
+
+        val result = flowHandle.use {
+            it.returnValue.getOrThrow()
+        }.tx.outputsOfType<ReinsuranceState>().single()
+
+        logger.info("Result is $result")
+
+        return ReinsuranceResponse(ledgerHashKey = result.linearId.id.toString(), contractId = result.contractId, message = "Registro atualizado com sucesso"
         )
     }
 
